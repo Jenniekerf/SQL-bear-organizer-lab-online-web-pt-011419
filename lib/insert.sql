becb9d13fa758d7a307f97b7b06082f3c3d7b653
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-03-20 15:08:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (2,"Rowdy", 12, "M", "black", "feisty", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (3,"Tabitha", 8, "F", "green", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (4,"Sergeant Brown", 9, "M", "white", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (5,"Melissa", 10, "F", "blue", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (6,"Grinch", 20, "M", "dark brown", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (7,"Wendy", 17, "M", "spotted", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (8,"NULL", 4, "F", "grey", "high", "0");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "M", "black", "feisty", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "F", "green", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, "M", "white", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "F", "blue", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 17, "M", "spotted", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", 4, "F", "grey", "high", "0");
